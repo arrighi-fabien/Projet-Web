e41bca98326c2be316770575efb748fd8bad9c98
Draak2A <fab.arrighi@gmail.com>
2023-03-10 20:34:45 +0100

    Creation of navbar and footer
    - Responsive
    - Animation for the navbar on phone

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav_menu = document.querySelector('.header__menu');
+const nav_link = document.querySelector('.header__nav');
+const nav__account = document.querySelector('.header__account');
+nav_menu.addEventListener('click', () => {
+    nav_menu.classList.toggle('header__menu--active');
+    document.body.classList.toggle('no-scroll');
+    nav_link.classList.toggle('header__nav--open');
+    nav__account.classList.toggle('header__account--open');
+})
