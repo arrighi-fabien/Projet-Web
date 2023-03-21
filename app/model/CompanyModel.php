180a155b3de3e4f48815320c11c4fc3e32a8e522
Draak2A <fab.arrighi@gmail.com>
2023-03-21 02:10:09 +0100

    Fix error on number of offer for a company

--- app/model/CompanyModel.php
+++ app/model/CompanyModel.php
@@ -3,7 +3,7 @@
 class CompanyModel extends Database {
 
     public function getBestCompanies() {
-        $result = $this->query("SELECT company.id_company, company_name, GROUP_CONCAT(DISTINCT city_name ORDER BY city_name SEPARATOR ', ') AS city, sector_name, COUNT(id_internship) AS offers FROM company JOIN work_at ON company.id_company = work_at.id_company JOIN city ON work_at.id_city = city.id_city JOIN sector ON company.id_sector = sector.id_sector LEFT JOIN internship ON company.id_company = internship.id_company WHERE is_visible = 1 GROUP BY company.id_company ORDER BY nb_student_accepted DESC LIMIT 4");
+        $result = $this->query("SELECT company.id_company, company_name, GROUP_CONCAT(DISTINCT city_name ORDER BY city_name SEPARATOR ', ') AS city, sector_name, COUNT(DISTINCT id_internship) AS offers FROM company JOIN work_at ON company.id_company = work_at.id_company JOIN city ON work_at.id_city = city.id_city JOIN sector ON company.id_sector = sector.id_sector LEFT JOIN internship ON company.id_company = internship.id_company WHERE is_visible = 1 GROUP BY company.id_company ORDER BY nb_student_accepted DESC LIMIT 4");
         return $result->fetchAll();
     }
 
