811ecb6de35f108fcce99201e3f2e8e8c67e229f
BennatFr <48688527+BennatFr@users.noreply.github.com>
2023-03-22 13:27:20 +0100

    view template

--- app/view/view.php
+++ app/view/view.php
@@ -2,6 +2,7 @@
 
 $smarty = new Smarty();
 $smarty->setTemplateDir('../app/view/templates');
+$smarty->setCompileDir('../app/view/templates_c');
 
 $smarty->assign('user', $_SESSION['user'] ?? false);
 
